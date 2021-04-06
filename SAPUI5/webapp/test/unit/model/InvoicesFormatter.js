e2ad5f1eb433745dd9e52f13cf1efbe8ac0c9223
Logali-Group <jalegriav@innoteamperu.com>
2021-04-06 02:48:21 +0000

    05 Estructura Proyectos SAPUI5

---
+++ SAPUI5/webapp/test/unit/model/InvoicesFormatter.js
@@ -0,0 +1,42 @@
+//@ts-nocheck
+sap.ui.define([
+    "logaligroup/SAPUI5/model/InvoicesFormatter",
+    "sap/ui/model/resource/ResourceModel"
+], 
+/**
+ * @param {typeof sap.ui.model.resource.ResourceModel} ResourceModel
+ */
+    function(InvoicesFormatter, ResourceModel ){
+
+        QUnit.module("Qnvoices Status", {
+            beforeEach: function() {
+                this._oResourceModel = new ResourceModel({
+                    bundleUrl : sap.ui.require.toUrl("logaligroup/SAPUI5") + "/i18/i18n.properties"
+                });
+            },
+
+            afterEach: function () {
+                this._oResourceModel.destroy();
+            }
+        });
+
+        QUnit.test("Should return the Invoices Status" , function(assert){
+            let oModel = this.stub();
+            oModel.withArgs("i18n").returns(this._oResourceModel);
+
+            let oViewStub = {
+                getModel : oModel
+            };
+
+            let oControllerStub = {
+                getView : this.stub().returns(oViewStub)
+            };
+
+            let fnIsolatedFormatter = InvoicesFormatter.invoiceStatus.bind(oControllerStub);
+
+            //Assert
+            assert.strictEqual(fnIsolatedFormatter("A"), "New", "The Invoices status for A is correct");
+            assert.strictEqual(fnIsolatedFormatter("B"), "In Progress", "The Invoices status for B is correct");
+            assert.strictEqual(fnIsolatedFormatter("C"), "Done", "The Invoices status for C is correct");
+        });
+});
