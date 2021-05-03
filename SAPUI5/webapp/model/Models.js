65f2253a40f04086ee907724abe4f49dd339baf0
Logali-Group <jalegriav@innoteamperu.com>
2021-05-03 15:31:32 +0000

    05 Estructura Proyectos SAPUI5

--- SAPUI5/webapp/model/Models.js
+++ SAPUI5/webapp/model/Models.js
@@ -1,11 +1,13 @@
 // @ts-nocheck
 sap.ui.define([ 
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/Device"
 ], 
 /** 
 * @param {typeof sap.ui.model.json.JSONModel} JSONModel
+* @param {typeof sap.ui.Device} Device
  */
-function (JSONModel) {
+function (JSONModel, Device) {
     "use strict";
     
     return {
@@ -17,6 +19,12 @@ function (JSONModel) {
             };
             
             return new JSONModel(oData);
+        },
+
+        createDeviceModel: function (){
+            var oModel = new JSONModel(Device);
+            oModel.setDefaultBindingMode("OneWay");
+            return oModel;
         }
     }
 });
